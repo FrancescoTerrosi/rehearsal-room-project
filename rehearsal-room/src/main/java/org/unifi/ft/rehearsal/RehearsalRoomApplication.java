7f523c3b4a03d6faf8437ece264b07e068dfae12
Francesco Terrosi <francesco.terrosi1@gmail.com>
2019-03-04 15:12:11 +0100

    Created new project

---
+++ rehearsal-room/src/main/java/org/unifi/ft/rehearsal/RehearsalRoomApplication.java
@@ -0,0 +1,13 @@
+package org.unifi.ft.rehearsal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RehearsalRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RehearsalRoomApplication.class, args);
+	}
+
+}
