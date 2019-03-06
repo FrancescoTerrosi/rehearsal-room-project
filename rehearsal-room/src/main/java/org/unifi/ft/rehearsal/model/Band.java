1cf5bad5cd6f28950c396573a1a6d3052c96224c
francesco <francesco.terrosi1@gmail.com>
2019-03-06 19:27:50 +0100

    Refactor + generated equals and hashcode

--- rehearsal-room/src/main/java/org/unifi/ft/rehearsal/model/Band.java
+++ rehearsal-room/src/main/java/org/unifi/ft/rehearsal/model/Band.java
@@ -20,5 +20,36 @@ public class Band {
 	public String getPassw() {
 		return passw;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((passw == null) ? 0 : passw.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Band other = (Band) obj;
+		if (passw == null) {
+			if (other.passw != null)
+				return false;
+		} else if (!passw.equals(other.passw))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
 	
 }
--- rehearsal-room/src/main/java/org/unifi/ft/rehearsal/model/Schedule.java
+++ rehearsal-room/src/main/java/org/unifi/ft/rehearsal/model/Schedule.java
@@ -33,5 +33,45 @@ public class Schedule {
 	public RehearsalRoom getRoom() {
 		return this.room;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((band == null) ? 0 : band.hashCode());
+		result = prime * result + ((endDate == null) ? 0 : endDate.hashCode());
+		result = prime * result + ((room == null) ? 0 : room.hashCode());
+		result = prime * result + ((startDate == null) ? 0 : startDate.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Schedule other = (Schedule) obj;
+		if (band == null) {
+			if (other.band != null)
+				return false;
+		} else if (!band.equals(other.band))
+			return false;
+		if (endDate == null) {
+			if (other.endDate != null)
+				return false;
+		} else if (!endDate.equals(other.endDate))
+			return false;
+		if (room != other.room)
+			return false;
+		if (startDate == null) {
+			if (other.startDate != null)
+				return false;
+		} else if (!startDate.equals(other.startDate))
+			return false;
+		return true;
+	}
 	
 }
