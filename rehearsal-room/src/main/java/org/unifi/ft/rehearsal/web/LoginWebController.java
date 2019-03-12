44affc0775f56760589e0d08bc4b54a0338cf481
francesco <francesco.terrosi1@gmail.com>
2019-03-12 21:37:18 +0100

    I forgot to remove an unused import: woops!

--- rehearsal-room/src/main/java/org/unifi/ft/rehearsal/web/LoginWebController.java
+++ rehearsal-room/src/main/java/org/unifi/ft/rehearsal/web/LoginWebController.java
@@ -3,7 +3,6 @@ package org.unifi.ft.rehearsal.web;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
