b82f25d7a5f4bad7f2fc569a98af2f2db12ddbef
francesco <francesco.terrosi1@gmail.com>
2019-03-21 15:35:50 +0100

    Started en2end testing

---
+++ rehearsal-room/src/ete-test/java/org/unifi/ft/rehearsal/features/IndexEndToEndBDD.java
@@ -0,0 +1,22 @@
+package org.unifi.ft.rehearsal.features;
+
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.unifi.ft.rehearsal.RehearsalRoomApplication;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/ete-test/resources/index.feature")
+public class IndexEndToEndBDD {
+
+	@BeforeClass
+	public static void setupClass() {
+		SpringApplicationBuilder builder = new SpringApplicationBuilder(RehearsalRoomApplication.class);
+		builder.headless(false);
+		builder.run(new String[] { "--server.port=11111" });
+	}
+	
+}
