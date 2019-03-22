de5675e853a5cc68644c5d5032ad75e8f6dae716
francesco <francesco.terrosi1@gmail.com>
2019-03-22 19:38:27 +0100

    Maybe this is the fix?

--- rehearsal-room/src/ete-test/java/org/unifi/ft/rehearsal/features/IndexEndToEndBDD.java
+++ rehearsal-room/src/ete-test/java/org/unifi/ft/rehearsal/features/IndexEndToEndBDD.java
@@ -1,12 +1,18 @@
 package org.unifi.ft.rehearsal.features;
 
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import io.github.bonigarcia.wdm.ChromeDriverManager;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/ete-test/resources/index.feature")
 public class IndexEndToEndBDD {
 
+	@BeforeClass
+	public static void setupClass() {
+		ChromeDriverManager.getInstance().setup();
+	}
 }
--- rehearsal-room/src/ete-test/java/org/unifi/ft/rehearsal/features/LoginPageEndToEndBDD.java
+++ rehearsal-room/src/ete-test/java/org/unifi/ft/rehearsal/features/LoginPageEndToEndBDD.java
@@ -1,11 +1,19 @@
 package org.unifi.ft.rehearsal.features;
+
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import io.github.bonigarcia.wdm.ChromeDriverManager;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/ete-test/resources/login.feature")
 public class LoginPageEndToEndBDD {
+
+	@BeforeClass
+	public static void setupClass() {
+		ChromeDriverManager.getInstance().setup();
+	}
 
 }
