62c99a1de1752be9f60db8dbd27543a10bb1cde2
francesco <francesco.terrosi1@gmail.com>
2019-03-23 17:33:34 +0100

    Created new scenarios

--- rehearsal-room/src/e2e/java/org/unifi/ft/rehearsal/features/steps/SchedulePageCucumberSteps.java
+++ rehearsal-room/src/e2e/java/org/unifi/ft/rehearsal/features/steps/SchedulePageCucumberSteps.java
@@ -86,24 +86,56 @@ public class SchedulePageCucumberSteps {
 
 	@Given("^The user is logged into the system$")
 	public void theUserIsLoggedIntoTheSystem() throws Throwable {
-		driver.get(HOMEPAGE+port+"/login");
+		driver.get(HOMEPAGE + port + "/login");
 		driver.findElement(By.id("username")).sendKeys("BandName");
 		driver.findElement(By.id("password")).sendKeys("BandPw");
 		driver.findElement(By.id("submit")).click();
 	}
 
+	@Given("^There are some schedules in the DB$")
+	public void thereAreSomeSchedulesInTheDB() throws Throwable {
+		DateTime startDate = new DateTime(2121, 12, 12, 12, 12, 0);
+		DateTime endDate = startDate.plusHours(Scheduler.HOUR_DURATION).plusMinutes(Scheduler.MINUTE_DURATION);
+		Schedule s = new Schedule("OneBand", startDate, endDate, RehearsalRoom.FIRSTROOM);
+		scheduleRepo.save(s);
+		assertEquals(1, scheduleRepo.count());
+	}
+
 	@When("^The user requests the /schedule url$")
 	public void the_user_requests_the_schedule_url() throws Throwable {
-		driver.get(HOMEPAGE+port+"/schedule");
-		assertEquals(HOMEPAGE+port+"/schedule", driver.getCurrentUrl());
+		driver.get(HOMEPAGE + port + "/schedule");
+		assertEquals(HOMEPAGE + port + "/schedule", driver.getCurrentUrl());
 	}
 
 	@When("^The user schedule for a free room in valid date$")
 	public void the_user_schedule_for_a_free_room_in_valid_date() throws Throwable {
 		WebElement scheduleDiv = driver.findElement(By.id("scheduleContent"));
-		submitSchedule(scheduleDiv, "2121","12","12","12","12","FIRSTROOM");
+		submitSchedule(scheduleDiv, "2121", "12", "12", "12", "12", "FIRSTROOM");
 	}
 
+	@When("^The user schedule for a free room in a day the does not exist$")
+	public void the_user_schedule_for_a_free_room_in_a_day_the_does_not_exist() throws Throwable {
+		WebElement scheduleDiv = driver.findElement(By.id("scheduleContent"));
+		submitSchedule(scheduleDiv, "2121", "2", "30", "12", "12", "FIRSTROOM");
+	}
+
+	@When("^The user schedule for a free room but leaving a blank field$")
+	public void the_user_schedule_for_a_free_room_but_leaving_a_blank_field() throws Throwable {
+		WebElement scheduleDiv = driver.findElement(By.id("scheduleContent"));
+		submitSchedule(scheduleDiv, "2121", "", "30", "12", "12", "FIRSTROOM");
+	}
+
+	@When("^The user schedule for a room that is not free$")
+	public void the_user_schedule_for_a_room_that_is_not_free() throws Throwable {
+		WebElement scheduleDiv = driver.findElement(By.id("scheduleContent"));
+		submitSchedule(scheduleDiv, "2121", "12", "12", "12", "12", "FIRSTROOM");
+	}
+
+	@When("^The user schedule for a room in a time t < \\(now \\+ (\\d+) minutes\\)$")
+	public void the_user_schedule_for_a_room_in_a_time_t_now_minutes(int arg1) throws Throwable {
+		WebElement scheduleDiv = driver.findElement(By.id("scheduleContent"));
+		submitSchedule(scheduleDiv, "2015", "12", "12", "12", "12", "FIRSTROOM");
+	}
 
 	@Then("^The request is accepted$")
 	public void the_request_is_accepted() throws Throwable {
@@ -113,19 +145,13 @@ public class SchedulePageCucumberSteps {
 	@Then("^The schedule is stored in the DB$")
 	public void the_schedule_is_stored_in_the_DB() throws Throwable {
 		assertEquals(1, scheduleRepo.count());
-		DateTime start = new DateTime(2121,12,12,12,12,0);
+		DateTime start = new DateTime(2121, 12, 12, 12, 12, 0);
 		DateTime end = start.plusHours(Scheduler.HOUR_DURATION).plusMinutes(Scheduler.MINUTE_DURATION);
 		Schedule s = scheduleRepo.findAll().get(0);
-		assertEquals(s.getBand(),"BandName");
-		assertEquals(s.getRoom(),RehearsalRoom.FIRSTROOM);
+		assertEquals(s.getBand(), "BandName");
+		assertEquals(s.getRoom(), RehearsalRoom.FIRSTROOM);
 		assertEquals(s.getStartDate(), start);
 		assertEquals(s.getEndDate(), end);
-	}
-
-	@When("^The user schedule for a free room in a day the does not exist$")
-	public void the_user_schedule_for_a_free_room_in_a_day_the_does_not_exist() throws Throwable {
-	    WebElement scheduleDiv = driver.findElement(By.id("scheduleContent"));
-	    submitSchedule(scheduleDiv, "2121", "2", "30", "12", "12", "FIRSTROOM");
 	}
 
 	@Then("^An invalid date message is shown$")
@@ -133,9 +159,24 @@ public class SchedulePageCucumberSteps {
 		assertEquals(SchedulePageWebController.NUMBER_ERROR_MESSAGE, driver.findElement(By.id("infos")).getText());
 	}
 
+	@Then("^A room not free message is shown$")
+	public void a_room_not_free_message_is_shown() throws Throwable {
+		assertEquals(SchedulePageWebController.ROOM_ERROR_MESSAGE, driver.findElement(By.id("infos")).getText());
+	}
+
+	@Then("^The schedule is not stored in the empty DB$")
+	public void theScheduleIsNotStoredInTheEmptyDB() throws Throwable {
+		assertEquals(0, scheduleRepo.count());
+	}
+
 	@Then("^The schedule is not stored in the DB$")
 	public void the_schedule_is_not_stored_in_the_DB() throws Throwable {
-		assertEquals(0, scheduleRepo.count());
+		assertEquals(1, scheduleRepo.count());
+	}
+
+	@Then("^A funny message is shown$")
+	public void a_funny_message_is_shown() throws Throwable {
+		assertEquals(SchedulePageWebController.TIME_ERROR_MESSAGE, driver.findElement(By.id("infos")).getText());
 	}
 
 	private void submitSchedule(WebElement div, String year, String month, String day, String hour, String minutes,
@@ -146,7 +187,7 @@ public class SchedulePageCucumberSteps {
 		div.findElement(By.id("hour")).sendKeys(hour);
 		div.findElement(By.id("minutes")).sendKeys(minutes);
 		div.findElement(By.id("room")).sendKeys(room);
-		div.findElement(By.id("submit")).click();	
+		div.findElement(By.id("submit")).click();
 	}
 
 	private BandDetails createTestUser() {
