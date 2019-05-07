c7503c098e07d4fe12f41c900d542833e9166c5b
FrancescoTerrosi <francesco.terrosi1@gmail.com>
2019-05-07 18:30:30 +0200

    Final push

--- rehearsal-room/src/main/java/org/unifi/ft/rehearsal/configurations/WebSecurityConfig.java
+++ rehearsal-room/src/main/java/org/unifi/ft/rehearsal/configurations/WebSecurityConfig.java
@@ -12,7 +12,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -26,7 +25,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	private static final String HOMEPAGE = "/schedule";
 	private static final String PROTECTED = "/schedule/**";
-	private static final String RESOURCES = "/resources/**";
 	private static final String[] PUBLIC_ACCESS_URIS = { "/", "/register"};
 
 	@Autowired
@@ -55,11 +53,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 			.permitAll();
 		
-	}
-
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers(RESOURCES);
 	}
 
 	@Override
